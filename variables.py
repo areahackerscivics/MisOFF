c23476057e4b322b2050d73ea0c866fe758f1d3d
Ricardo Cancar <ricardocancar@gmail.com>
2018-02-28 13:34:24 +0100

    Configuration file

---
+++ variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+## en esté archivo se guardan las variables necesarias para correr el programa.
+
+Token_bot = 'xxxx'
+url_server = 'url'
+user_mongo = 'xxxx'
+passw_mongo = 'xxxx'
